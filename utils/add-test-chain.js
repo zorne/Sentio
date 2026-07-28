df923df7a14c12b4b4d7d87095de61a81b6888d1
zorne <zorne92@gmail.com>
2026-07-28 16:52:36 +0200

    sentio-dev: mise à jour de utils/add-test-chain.js

---
+++ utils/add-test-chain.js
@@ -0,0 +1,12 @@
+/**
+ * Additionne deux nombres.
+ *
+ * @param {number} a - Le premier nombre.
+ * @param {number} b - Le deuxième nombre.
+ * @returns {number} La somme de a et b.
+ */
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
